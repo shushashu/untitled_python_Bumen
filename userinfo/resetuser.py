f7ebb0c7e6c3c52d592dcf4ae5b839165b66c2fd
heroH <herohu@shunshunliuxue.com>
2018-11-16 15:06:39 +0800

    reset user

--- userinfo/resetuser.py
+++ userinfo/resetuser.py
@@ -4,13 +4,10 @@ from userinfo import user
 # 重置账户所需的参数
 username = "test027"
 password = "678975765"
-token = BuMenSDK.access_Token()
-trade_no = BuMenSDK.createTradeNo("user")
 
-
-re = user.resetuser(trade_no=trade_no, token=token["access_token"], username=username, password=password)
+re = user.ResetUser(username=username, password=password)
 a = re.resetuser()
 if a["err_code"] == "0":
-    re.sql()
+    print(a)
 else:
     print(a["err_code"])
